693db96582eab39f93c42ea45c5f6b5210142a50
isnotsin <imnotsin18@gmail.com>
2025-10-16 12:12:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESCRIPTION = (THIS_DIR / "README.md").read_text()
 
 setup(
     name=PACKAGE,
-    version='0.0.7',
+    version='0.0.8',
     description="Sin Checker",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
